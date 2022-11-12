cb03a0f9bbb610d0b881f08e68599e208b912074
nazar silkin <ipz212_sna@student.ztu.edu.ua>
2022-11-12 17:01:19 +0100

    Adding output message

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -7,6 +7,8 @@ namespace Task1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
