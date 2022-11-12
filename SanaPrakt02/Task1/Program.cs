15af0f9410b8bfb7817e7c6811143e0302a4d5ed
nazar silkin <ipz212_sna@student.ztu.edu.ua>
2022-11-12 16:53:59 +0100

    Changing output text

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -6,7 +6,7 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sana Commerce Software Development Bootcamp!");
         }
     }
 }
